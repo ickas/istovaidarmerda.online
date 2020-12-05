2fb591cc245407fb863e0f745be2c706105772dd
João Dias <joao.dias@feedzai.com>
2020-12-05 01:26:49 +0000

    feat: Added testing tools to the codebase
    Added  jest, testing-library and cypress

---
+++ cypress/support/index.js
@@ -0,0 +1,15 @@
+/**
+ * index.js
+ *
+ * Cypress Support file
+ *
+ * @author João Dias <contacto@joaodias.me>
+ * @since 0.0.1
+ */
+
+// Cypress Plugins
+import "@cypress/code-coverage/support";
+import "cypress-plugin-tab";
+
+// Custom Commands
+import "./commands";
