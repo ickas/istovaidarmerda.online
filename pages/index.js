9fd5de9bfbedf034551cab69038161b1d569a742
Henrique Macedo <mail@henriquemacedo.pt>
2020-12-20 22:22:42 +0000

    add schedule section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
 import Hero from "components/Hero";
+import Schedule from "components/Schedule";
+import { events } from "../utils/schedule";
 
 export default function Home() {
 	return (
@@ -13,6 +15,7 @@ export default function Home() {
 			</Head>
 
 			<Hero />
+			<Schedule events={events} />
 		</>
 	);
 }
