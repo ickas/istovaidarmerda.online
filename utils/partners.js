077f50cd2b74a401e9e6b29ab923da587a5201a9
Henrique Macedo <mail@henriquemacedo.pt>
2020-12-22 01:10:38 +0000

    add partners logos
    - partners data
    - partners logos

---
+++ utils/partners.js
@@ -0,0 +1,26 @@
+export const partners = [
+  {
+    bg: "#E53935",
+    name: "market[ING]",
+    logo: "/images/partners/marketing-logo.svg",
+    url: "https://inglesparamarketing.com",
+  },
+  {
+    bg: "#ffffff",
+    name: "Nacional 2 Podcast",
+    logo: "/images/partners/n2-podcast.svg",
+    url: "https://nacional2podcast.pt",
+  },
+  {
+    bg: "#1C1C1E",
+    name: "iStuff",
+    logo: "/images/partners/istuff.png",
+    url: "https://istuff.pt",
+  },
+  {
+    bg: "#ffffff",
+    name: "Docc Media",
+    logo: "/images/partners/doccmedia.png",
+    url: "https://facebook.com/doccmedia",
+  },
+];
