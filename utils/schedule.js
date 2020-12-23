51b3dcbc907d271048f7c383d4c73192e9e6dfd1
Henrique Macedo <mail@henriquemacedo.pt>
2020-12-23 22:51:42 +0000

    change pedro brinca twitter handle

--- utils/schedule.js
+++ utils/schedule.js
@@ -1,296 +1,296 @@
 export const events = [
-	{
-		date: "12 Jan",
-		area: "Profissionais da Saúde e da Ciência",
-		title: "Os Heróis da Pandemia",
-		moderator: [
-			{
-				avatar: "/images/avatars/bernardo-gomes.jpg",
-				name: "Bernardo Gomes",
-				job: "Médico de Saúde Pública",
-				url: "https://twitter.com/BLMG",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/joana-novo.jpg",
-				name: "Joana Novo",
-				job: "Médica de Família",
-				url: "https://twitter.com/banana_new",
-			},
-			{
-				avatar: "/images/avatars/paulo-baltazar.jpg",
-				name: "Paulo Baltazar",
-				job: "Enfermeiro Hospitalar",
-				url: "https://twitter.com/7Balthazar",
-			},
-			{
-				avatar: "/images/avatars/joao-moreira-pinto.jpg",
-				name: "João Moreira Pinto",
-				job: "Cirurgião Pediátrico",
-				url: "https://twitter.com/joaompinto",
-			},
-		],
-	},
-	{
-		date: "14 Jan",
-		area: "Primeira e Terceira Idades",
-		title: "A Pandemia do 8 ao 80",
-		moderator: [
-			{
-				avatar: "/images/avatars/sonia-teles-fernandes.jpg",
-				name: "Sónia Teles Fernandes",
-				job: "Criadora do Isto Vai Dar Merda",
-				url: "https://twitter.com/SoniaTelesF",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/a-john.jpg",
-				name: "A_John",
-				job: "Diretora de Colégia Particular",
-				url: "https://twitter.com/a_john",
-			},
-			{
-				avatar: "/images/avatars/vanessa-maltez-amado.jpg",
-				name: "Vanessa Maltez Amado",
-				job: "Educadora de Infância",
-				url: "https://twitter.com/EducadoraNessa",
-			},
-			{
-				avatar: "/images/avatars/carla-catarina-neves.jpg",
-				name: "Carla Catarina Neves",
-				job: "Cuidadora Informal",
-				url: "https://twitter.com/KatKatrinaNeves",
-			},
-			{
-				avatar: "/images/avatars/maria-joao-amorim.jpg",
-				name: "Maria João Amorim",
-				job: "Virologista",
-				url: "https://twitter.com/mjamorim1",
-			},
-		],
-	},
-	{
-		date: "19 Jan",
-		area: "Cultura e Artes",
-		title: "Dos aplausos ao silêncio",
-		moderator: [
-			{
-				avatar: "/images/avatars/miguel-bica.jpg",
-				name: "Miguel Bica",
-				job: "Fundador plataforma Gerador",
-				url: "https://twitter.com/migbica",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/fernando-ribeiro.jpg",
-				name: "Fernando Ribeiro",
-				job: "Moonspell",
-				url: "https://twitter.com/moonspell",
-			},
-			{
-				avatar: "/images/avatars/katia-guerreiro.jpg",
-				name: "Katia Guerreiro",
-				job: "Fadista",
-				url: "https://facebook.com/katiaguerreiro.official",
-			},
-			{
-				avatar: "/images/avatars/antonio-machado.jpg",
-				name: "António Machado",
-				job: "Ator, Comediante",
-				url: "https://facebook.com/actorantoniomachado",
-			},
-			{
-				avatar: "/images/avatars/hugo-nobrega.jpg",
-				name: "Hugo Nóbrega",
-				job: "Produtor",
-				url: "https://twitter.com/nobregahugo",
-			},
-		],
-	},
-	{
-		date: "21 Jan",
-		area: "Economia",
-		title: "O ROI de uma Pandemia",
-		moderator: [
-			{
-				avatar: "/images/avatars/luis-aguiar-conraria.jpg",
-				name: "Luís Aguiar-Conraria",
-				job: "Economista, Cronista",
-				url: "https://twitter.com/LConraria",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/pedro-brinca.jpg",
-				name: "Pedro Brinca",
-				job: "Economista",
-				url: "https://twitter.com/PMSBrinca",
-			},
-			{
-				avatar: "/images/avatars/susana-peralta.jpg",
-				name: "Susana Peralta",
-				job: "Economista, Jornalista",
-				url: "https://twitter.com/speraltalisboa",
-			},
-			{
-				avatar: "/images/avatars/jorge-pimenta.jpg",
-				name: "Jorge Pimenta",
-				job: "Empreendedorismo",
-				url: "https://twitter.com/jdpimenta",
-			},
-			{
-				avatar: "/images/avatars/jorge-faustino.jpg",
-				name: "Jorge Faustino",
-				job: "Marketing, Publicidade",
-				url: "https://twitter.com/jorgefaustino",
-			},
-		],
-	},
-	{
-		date: "26 Jan",
-		area: "Saúde Pública",
-		title: "Quando a Saúde fica doente",
-		moderator: [
-			{
-				avatar: "/images/avatars/vasco-m-barreto.jpg",
-				name: "Vasco M. Barreto",
-				job: "Biólogo, Investigador",
-				url: "https://twitter.com/VascoMBarreto1",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/mario-macedo.jpg",
-				name: "Mário Macedo",
-				job: "Enfermeiro, Saúde Pública e Infantil",
-				url: "https://twitter.com/Mmacedo_tweets",
-			},
-			{
-				avatar: "/images/avatars/pedro-morgado.jpg",
-				name: "Pedro Morgado",
-				job: "Psiquiatra",
-				url: "https://twitter.com/pedromorgado",
-			},
-			{
-				avatar: "/images/avatars/david-rodrigues.jpg",
-				name: "David Rodrigues",
-				job: "Médico, Investigador",
-				url: "https://twitter.com/davsrodrigues",
-			},
-		],
-	},
-	{
-		date: "28 Jan",
-		area: "Media",
-		title: "Somos capazes de lidar com a verdade?",
-		moderator: [
-			{
-				avatar: "/images/avatars/pedro-boucherie-mendes.jpg",
-				name: "Pedro Boucherie Mendes",
-				job: "Jornalista, Autor",
-				url: "https://twitter.com/pedroboucherie",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/fernanda-freitas.jpg",
-				name: "Fernanda Freitas",
-				job: "Apresentadora, Formadora em Media",
-				url: "https://twitter.com/FernandaFreitas",
-			},
-			{
-				avatar: "/images/avatars/ana-delicado.jpg",
-				name: "Ana Delicado",
-				job: "Investigadora Estudos Sociais da Ciência",
-				url: "https://twitter.com/anadelicado3",
-			},
-			{
-				avatar: "/images/avatars/carlos-rodrigues-lima.jpg",
-				name: "Carlos Rodrigues Lima",
-				job: "Jornalista",
-				url: "https://twitter.com/CarlosLima7",
-			},
-		],
-	},
-	{
-		date: "2 Fev",
-		area: "Comunicação de Crise",
-		title: "A (boa) comunicação pode acabar com uma Pandemia?",
-		moderator: [
-			{
-				avatar: "/images/avatars/maria-joao-nogueira.jpg",
-				name: "Maria João Nogueira",
-				job: "Comunicação, Social Media",
-				url: "https://twitter.com/jonasnuts",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/pedro-aniceto.jpg",
-				name: "Pedro Aniceto",
-				job: "Consultor, Comunicação, Autor",
-				url: "https://twitter.com/pedroaniceto",
-			},
-			{
-				avatar: "/images/avatars/marco-maia.jpg",
-				name: "Marco Maia",
-				job: "vost.pt",
-				url: "https://twitter.com/marcoacmaia",
-			},
-			{
-				avatar: "/images/avatars/joana-lobo-antunes.jpg",
-				name: "Joana Lobo Antunes",
-				job: "Comunicação de Ciência",
-				url: "https://twitter.com/JoanaLoA",
-			},
-			{
-				avatar: "/images/avatars/silvia-castro.jpg",
-				name: "Sílvia Castro",
-				job: "Neurociências, Comunicação de Ciência",
-				url: "https://twitter.com/Silvia_Castro",
-			},
-		],
-	},
-	{
-		date: "4 Fev",
-		area: "Política",
-		title: "A Política e Politiquices de uma Pandemia",
-		moderator: [
-			{
-				avatar: "/images/avatars/pedro-magalhaes.jpg",
-				name: "Pedro Magalhães",
-				job: "Ciência Política",
-				url: "https://twitter.com/PCMagalhaes",
-			},
-		],
-		speakers: [
-			{
-				avatar: "/images/avatars/carmo-afonso.jpg",
-				name: "Carmo Afonso",
-				job: "Advogada, Cronista",
-				url: "https://twitter.com/carmoafonso",
-			},
-			{
-				avatar: "/images/avatars/miguel-poiares-maduro.jpg",
-				name: "Miguel Poiares Maduro",
-				job: "Político, Comentador",
-				url: "https://twitter.com/MaduroPoiares",
-			},
-			{
-				avatar: "/images/avatars/vasco-m-barreto.jpg",
-				name: "Vasco M. Barreto",
-				job: "Biólogo, Investigador",
-				url: "https://twitter.com/VascoMBarreto1",
-			},
-			{
-				avatar: "/images/avatars/tiago-correia.jpg",
-				name: "Tiago Correia",
-				job: "Professor, Investigador - Saúde Pública Internacional",
-				url: "https://facebook.com/tigcorreia",
-			},
-		],
-	},
+  {
+    date: "12 Jan",
+    area: "Profissionais da Saúde e da Ciência",
+    title: "Os Heróis da Pandemia",
+    moderator: [
+      {
+        avatar: "/images/avatars/bernardo-gomes.jpg",
+        name: "Bernardo Gomes",
+        job: "Médico de Saúde Pública",
+        url: "https://twitter.com/BLMG",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/joana-novo.jpg",
+        name: "Joana Novo",
+        job: "Médica de Família",
+        url: "https://twitter.com/banana_new",
+      },
+      {
+        avatar: "/images/avatars/paulo-baltazar.jpg",
+        name: "Paulo Baltazar",
+        job: "Enfermeiro Hospitalar",
+        url: "https://twitter.com/7Balthazar",
+      },
+      {
+        avatar: "/images/avatars/joao-moreira-pinto.jpg",
+        name: "João Moreira Pinto",
+        job: "Cirurgião Pediátrico",
+        url: "https://twitter.com/joaompinto",
+      },
+    ],
+  },
+  {
+    date: "14 Jan",
+    area: "Primeira e Terceira Idades",
+    title: "A Pandemia do 8 ao 80",
+    moderator: [
+      {
+        avatar: "/images/avatars/sonia-teles-fernandes.jpg",
+        name: "Sónia Teles Fernandes",
+        job: "Criadora do Isto Vai Dar Merda",
+        url: "https://twitter.com/SoniaTelesF",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/a-john.jpg",
+        name: "A_John",
+        job: "Diretora de Colégia Particular",
+        url: "https://twitter.com/a_john",
+      },
+      {
+        avatar: "/images/avatars/vanessa-maltez-amado.jpg",
+        name: "Vanessa Maltez Amado",
+        job: "Educadora de Infância",
+        url: "https://twitter.com/EducadoraNessa",
+      },
+      {
+        avatar: "/images/avatars/carla-catarina-neves.jpg",
+        name: "Carla Catarina Neves",
+        job: "Cuidadora Informal",
+        url: "https://twitter.com/KatKatrinaNeves",
+      },
+      {
+        avatar: "/images/avatars/maria-joao-amorim.jpg",
+        name: "Maria João Amorim",
+        job: "Virologista",
+        url: "https://twitter.com/mjamorim1",
+      },
+    ],
+  },
+  {
+    date: "19 Jan",
+    area: "Cultura e Artes",
+    title: "Dos aplausos ao silêncio",
+    moderator: [
+      {
+        avatar: "/images/avatars/miguel-bica.jpg",
+        name: "Miguel Bica",
+        job: "Fundador plataforma Gerador",
+        url: "https://twitter.com/migbica",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/fernando-ribeiro.jpg",
+        name: "Fernando Ribeiro",
+        job: "Moonspell",
+        url: "https://twitter.com/moonspell",
+      },
+      {
+        avatar: "/images/avatars/katia-guerreiro.jpg",
+        name: "Katia Guerreiro",
+        job: "Fadista",
+        url: "https://facebook.com/katiaguerreiro.official",
+      },
+      {
+        avatar: "/images/avatars/antonio-machado.jpg",
+        name: "António Machado",
+        job: "Ator, Comediante",
+        url: "https://facebook.com/actorantoniomachado",
+      },
+      {
+        avatar: "/images/avatars/hugo-nobrega.jpg",
+        name: "Hugo Nóbrega",
+        job: "Produtor",
+        url: "https://twitter.com/nobregahugo",
+      },
+    ],
+  },
+  {
+    date: "21 Jan",
+    area: "Economia",
+    title: "O ROI de uma Pandemia",
+    moderator: [
+      {
+        avatar: "/images/avatars/luis-aguiar-conraria.jpg",
+        name: "Luís Aguiar-Conraria",
+        job: "Economista, Cronista",
+        url: "https://twitter.com/LConraria",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/pedro-brinca.jpg",
+        name: "Pedro Brinca",
+        job: "Economista",
+        url: "https://twitter.com/pedro_brinca",
+      },
+      {
+        avatar: "/images/avatars/susana-peralta.jpg",
+        name: "Susana Peralta",
+        job: "Economista, Jornalista",
+        url: "https://twitter.com/speraltalisboa",
+      },
+      {
+        avatar: "/images/avatars/jorge-pimenta.jpg",
+        name: "Jorge Pimenta",
+        job: "Empreendedorismo",
+        url: "https://twitter.com/jdpimenta",
+      },
+      {
+        avatar: "/images/avatars/jorge-faustino.jpg",
+        name: "Jorge Faustino",
+        job: "Marketing, Publicidade",
+        url: "https://twitter.com/jorgefaustino",
+      },
+    ],
+  },
+  {
+    date: "26 Jan",
+    area: "Saúde Pública",
+    title: "Quando a Saúde fica doente",
+    moderator: [
+      {
+        avatar: "/images/avatars/vasco-m-barreto.jpg",
+        name: "Vasco M. Barreto",
+        job: "Biólogo, Investigador",
+        url: "https://twitter.com/VascoMBarreto1",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/mario-macedo.jpg",
+        name: "Mário Macedo",
+        job: "Enfermeiro, Saúde Pública e Infantil",
+        url: "https://twitter.com/Mmacedo_tweets",
+      },
+      {
+        avatar: "/images/avatars/pedro-morgado.jpg",
+        name: "Pedro Morgado",
+        job: "Psiquiatra",
+        url: "https://twitter.com/pedromorgado",
+      },
+      {
+        avatar: "/images/avatars/david-rodrigues.jpg",
+        name: "David Rodrigues",
+        job: "Médico, Investigador",
+        url: "https://twitter.com/davsrodrigues",
+      },
+    ],
+  },
+  {
+    date: "28 Jan",
+    area: "Media",
+    title: "Somos capazes de lidar com a verdade?",
+    moderator: [
+      {
+        avatar: "/images/avatars/pedro-boucherie-mendes.jpg",
+        name: "Pedro Boucherie Mendes",
+        job: "Jornalista, Autor",
+        url: "https://twitter.com/pedroboucherie",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/fernanda-freitas.jpg",
+        name: "Fernanda Freitas",
+        job: "Apresentadora, Formadora em Media",
+        url: "https://twitter.com/FernandaFreitas",
+      },
+      {
+        avatar: "/images/avatars/ana-delicado.jpg",
+        name: "Ana Delicado",
+        job: "Investigadora Estudos Sociais da Ciência",
+        url: "https://twitter.com/anadelicado3",
+      },
+      {
+        avatar: "/images/avatars/carlos-rodrigues-lima.jpg",
+        name: "Carlos Rodrigues Lima",
+        job: "Jornalista",
+        url: "https://twitter.com/CarlosLima7",
+      },
+    ],
+  },
+  {
+    date: "2 Fev",
+    area: "Comunicação de Crise",
+    title: "A (boa) comunicação pode acabar com uma Pandemia?",
+    moderator: [
+      {
+        avatar: "/images/avatars/maria-joao-nogueira.jpg",
+        name: "Maria João Nogueira",
+        job: "Comunicação, Social Media",
+        url: "https://twitter.com/jonasnuts",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/pedro-aniceto.jpg",
+        name: "Pedro Aniceto",
+        job: "Consultor, Comunicação, Autor",
+        url: "https://twitter.com/pedroaniceto",
+      },
+      {
+        avatar: "/images/avatars/marco-maia.jpg",
+        name: "Marco Maia",
+        job: "vost.pt",
+        url: "https://twitter.com/marcoacmaia",
+      },
+      {
+        avatar: "/images/avatars/joana-lobo-antunes.jpg",
+        name: "Joana Lobo Antunes",
+        job: "Comunicação de Ciência",
+        url: "https://twitter.com/JoanaLoA",
+      },
+      {
+        avatar: "/images/avatars/silvia-castro.jpg",
+        name: "Sílvia Castro",
+        job: "Neurociências, Comunicação de Ciência",
+        url: "https://twitter.com/Silvia_Castro",
+      },
+    ],
+  },
+  {
+    date: "4 Fev",
+    area: "Política",
+    title: "A Política e Politiquices de uma Pandemia",
+    moderator: [
+      {
+        avatar: "/images/avatars/pedro-magalhaes.jpg",
+        name: "Pedro Magalhães",
+        job: "Ciência Política",
+        url: "https://twitter.com/PCMagalhaes",
+      },
+    ],
+    speakers: [
+      {
+        avatar: "/images/avatars/carmo-afonso.jpg",
+        name: "Carmo Afonso",
+        job: "Advogada, Cronista",
+        url: "https://twitter.com/carmoafonso",
+      },
+      {
+        avatar: "/images/avatars/miguel-poiares-maduro.jpg",
+        name: "Miguel Poiares Maduro",
+        job: "Político, Comentador",
+        url: "https://twitter.com/MaduroPoiares",
+      },
+      {
+        avatar: "/images/avatars/vasco-m-barreto.jpg",
+        name: "Vasco M. Barreto",
+        job: "Biólogo, Investigador",
+        url: "https://twitter.com/VascoMBarreto1",
+      },
+      {
+        avatar: "/images/avatars/tiago-correia.jpg",
+        name: "Tiago Correia",
+        job: "Professor, Investigador - Saúde Pública Internacional",
+        url: "https://facebook.com/tigcorreia",
+      },
+    ],
+  },
 ];
