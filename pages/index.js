a05204e399590e57a7cd72780c5f49e77ccdd6fe
Henrique Macedo <mail@henriquemacedo.pt>
2020-12-28 19:23:21 +0000

    add seo

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,34 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Isto vai dar merda 💩</title>
-        <meta name="robots" content="noindex, nofollow" />
-        <meta key="description" name="description" content="" />
-        <meta key="keywords" name="keywords" content="" />
+        <meta charset="utf-8" />
+        <title>Isto Vai Dar Merda – na Pandemia</title>
+        <meta
+          key="description"
+          name="description"
+          content="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
+        />
+        <meta key="keywords" name="keywords" content="pandemia, covid, conferência, evento online" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:url" content="https://istovaidarmerda.online" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Isto Vai Dar Merda – na Pandemia" />
+        <meta
+          property="og:description"
+          content="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
+        />
+        <meta property="og:image" content="/images/ivdm-preview.png" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="672" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@IstoVai" />
+        <meta name="twitter:title" content="Isto Vai Dar Merda – na Pandemia" />
+        <meta
+          key="twitter:description"
+          name="twitter:description"
+          content="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
+        />
+        <meta name="twitter:image" content="/images/ivdm-preview.png" />
       </Head>
 
       <Hero />
