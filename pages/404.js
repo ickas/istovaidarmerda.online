1523ec7a80e49aafee5083b40b787e9bdd30c428
Henrique Macedo <mail@henriquemacedo.pt>
2020-12-29 21:34:08 +0000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,10 @@ import NotFound from "components/page-not-found";
 export default function PageNotFound() {
   return (
     <>
-      <SiteHead />
+      <SiteHead
+        title="Isto Deu Mesmo Merda"
+        description="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
+      />
       <NotFound />
     </>
   );
