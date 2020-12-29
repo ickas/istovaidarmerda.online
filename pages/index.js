d3399f0d038fe0b08d7c2958e9256440d3b21f36
Henrique Macedo <mail@henriquemacedo.pt>
2020-12-29 18:15:18 +0000

    add site head component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import SiteHead from "components/head";
 import Hero from "components/hero";
 import Schedule from "components/schedule";
 import Where from "components/where";
@@ -17,55 +17,10 @@ import { partners } from "../utils/partners";
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>Isto Vai Dar Merda – na Pandemia</title>
-        <meta
-          key="description"
-          name="description"
-          content="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
-        />
-        <meta key="keywords" name="keywords" content="pandemia, covid, conferência, evento online" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:url" content="https://istovaidarmerda.online" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Isto Vai Dar Merda – na Pandemia" />
-        <meta
-          property="og:description"
-          content="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
-        />
-        <meta property="og:image" content="/images/ivdm-preview.png" />
-        <meta property="og:image:width" content="1280" />
-        <meta property="og:image:height" content="672" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@IstoVai" />
-        <meta name="twitter:title" content="Isto Vai Dar Merda – na Pandemia" />
-        <meta
-          key="twitter:description"
-          name="twitter:description"
-          content="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
-        />
-        <meta name="twitter:image" content="/images/ivdm-preview.png" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/images/favicon/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/images/favicon/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/images/favicon/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/images/favicon/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/images/favicon/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/images/favicon/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/images/favicon/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
-        <link rel="shortcut icon" type="image/jpg" href="/images/favicon/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-
+      <SiteHead
+        title="Isto Vai Dar Merda – na Pandemia"
+        description="Criado com o intuito de fornecer um espaço onde os assuntos mais prementes possam ser discutidos de forma mais aberta, honesta e sem pressões."
+      />
       <Hero />
       <Schedule agenda={agenda} rubrics={rubrics} />
       <Where twitter="https://twitter.com/IstoVai" youtube="https://www.youtube.com/channel/UC-M0MHRa0CNewKhjCz4c2Qg" />
