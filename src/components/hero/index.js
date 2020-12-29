3f9719a5c155365edf40d4a6b49bc8752a3ec704
João Dias <joao.dias@feedzai.com>
2020-12-29 00:40:37 +0000

    Changed hero width detection to CSS to improve performance

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -10,7 +10,6 @@ export const WIDTH_BREAKPOINT = "(min-width: 700px)";
 export const MOUSE_BREAKPOINT = "(min-width: 538px) and (min-height: 812px)";
 
 const Hero = () => {
-  const widthBreakpoint = useBreakpoints("(min-width: 700px)");
   const mouseBreakpoint = useBreakpoints("(min-width: 538px) and (min-height: 720px)");
 
   const { ref, inView } = useInView({
@@ -33,21 +32,15 @@ const Hero = () => {
           <img src={src} srcSet={srcSet} width="302" height="333" alt="Logótipo do Isto Vai Dar Merda" loading="lazy" />
         </Styles.Logo>
         <Styles.Intro>
-          {widthBreakpoint ? (
-            <h1 className="hero__title" aria-label="Isto Vai Dar Merda" data-testid="hero-title">
-              <svg xmlns="http://www.w3.org/2000/svg" width={498} height={85}>
-                <text x={0} y={65}>
-                  Isto Vai Dar
-                </text>
-              </svg>
-              <span className="hero__title__markup">Merda</span>
-            </h1>
-          ) : (
-            <h1 className="hero__title" aria-label="Isto Vai Dar Merda" data-testid="hero-title">
-              <span className="hero__title__stroke">Isto Vai Dar</span>
-              <span className="hero__title__markup">Merda</span>
-            </h1>
-          )}
+          <h1 className="hero__title" aria-label="Isto Vai Dar Merda" data-testid="hero-title">
+            <svg className="hero__title__vector" xmlns="http://www.w3.org/2000/svg" width={498} height={85}>
+              <text x={0} y={65}>
+                Isto Vai Dar
+              </text>
+            </svg>
+            <span className="hero__title__stroke">Isto Vai Dar</span>
+            <span className="hero__title__markup">Merda</span>
+          </h1>
 
           <p className="hero__description" data-testid="hero-description">
             O <i>Isto Vai Dar Merda</i> é um evento que materializa a máxima “quem avisa, amigo é”, pensado para abordar
